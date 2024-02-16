340322acff6b5002a4b82e987b68a6af4cdb4be1
LiamLuinenburg <142930070+LiamLuinenburg@users.noreply.github.com>
2024-02-16 10:19:34 -0800

    hello github

---
+++ settings.py
@@ -0,0 +1,18 @@
+# This file was created by Liam Luinenburg
+TITLE = "my fun game"
+
+FPS = 30
+
+PLAYER_SPEED = 300
+
+WIDTH = 1024
+HEIGHT = 768
+TILESIZE = 32
+
+SKYBLUE = (111, 186, 234)
+
+LIGHTGREY = (100,100,100)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+BGCOLOR = SKYBLUE
